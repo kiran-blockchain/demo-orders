431f7aed6bb661de875bf97b7dfd478f72ccb76c
kiran-blockchain <kiran.pautri@brugu.io>
2021-02-12 08:12:10 +0530

    Contract Changed

--- src/modules/Order.ts
+++ src/modules/Order.ts
@@ -10,7 +10,8 @@ export function getAddressByNetwork(network: string): Address {
     return Address.fromString('0xd412054cca18a61278ced6f674a526a6940ebd84')
   } if (network == 'rinkeby') {
    // return Address.fromString('0xd412054cca18a61278ced6f674a526a6940ebd84')
-   return Address.fromString('0xdbaae94d69743c1afae083f1d08fd44f337ad65a')
+   //return Address.fromString('0xdbaae94d69743c1afae083f1d08fd44f337ad65a')
+   return Address.fromString('0xc746334cf670a5902c05a9E71b71BBF5e575f4ec')
   }
 
   throw 'No Address specified'
